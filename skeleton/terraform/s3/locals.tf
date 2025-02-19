dab528bd8290fc4d879411bc5db1547538c8fd21
Vijay John Stephen <vijay.stephen@sourcefuse.com>
2025-02-19 21:27:14 +0530

    added S3 module

--- skeleton/terraform/s3/locals.tf
+++ skeleton/terraform/s3/locals.tf
@@ -2,8 +2,8 @@ locals {
 
   buckets = {
     "${var.namespace}-${var.environment}-bucket1" = {
-      name                      = "${var.namespace}-${var.environment}-bucket1"
-      acl                       = "private"
+      name              = "${var.namespace}-${var.environment}-bucket1"
+      acl               = "private"
       enable_versioning = trie
     }
   }
